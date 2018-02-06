b268133f356894bc47b7c5d799dde98b20fad755
Kelsey D'Souza <kad2185@columbia.edu>
2018-02-06 12:51:19 -0500

    Add default caffe model and proto.

--- Project/detection_example.py
+++ Project/detection_example.py
@@ -8,9 +8,11 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
 ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
+	help="path to Caffe 'deploy' prototxt file", 
+	default='models/MobileNetSSD_deploy.prototxt.txt')
 ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
+	help="path to Caffe pre-trained model",
+	default="models/MobileNetSSD_deploy.caffemodel")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
