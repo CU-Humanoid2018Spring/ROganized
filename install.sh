d869d967622f829ba18081f90f6e4e6900a65bd7
YanS0ng <c.yansong@columbia.edu>
2018-04-09 14:50:00 -0400

    cv2 works with python3 + rospy

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo pip3 install catkin_pkg
 sudo pip3 install pymorse
 sudo pip3 install rospkg
 
+
+sudo ln -sf /usr/local/lib/python3.5/site-packages/cv2.so /usr/local/lib/python3.5/site-packages/cv2.so
