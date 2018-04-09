6a53bd66b6226b1529e31be9101d1542a4fe7abc
YanS0ng <c.yansong@columbia.edu>
2018-04-09 14:59:39 -0400

    Install missing opencv-python

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo pip3 install catkin_pkg
 sudo pip3 install pymorse
 sudo pip3 install rospkg
 
-
-sudo ln -sf /usr/local/lib/python3.5/site-packages/cv2.so /usr/local/lib/python3.5/site-packages/cv2.so
+sudo pip3 install opencv-python
+sudo ln -sf /usr/local/lib/python3.5/site-packages/cv2.so /usr/local/lib/python3.5/dist-packages/cv2.so
