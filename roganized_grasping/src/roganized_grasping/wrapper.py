e47c2cebd16754679e6b3336f70b7ace78fa92a8
YanS0ng <c.yansong@columbia.edu>
2018-04-17 17:14:21 -0400

    feat: Wrap graspit functions

--- roganized_grasping/src/roganized_grasping/wrapper.py
+++ roganized_grasping/src/roganized_grasping/wrapper.py
@@ -4,28 +4,6 @@ from graspit_commander import GraspitCommander
 import rospy
 from roganized_rl.utils import ImageSubscriber, GazeboClient, gen_pose, gen_rand_pose
 
-def demo():
-    gc = GraspitCommander()
-    gc.clearWorld()
-
-    # Import models
-    mug_pose = Pose(position=Point(0.0,0,0),orientation=Quaternion(0.707,0,0,-0.707))
-    table_pose = Pose(position=Point(-0.2,-0.2,0.48),orientation=Quaternion(0,0,0,1))
-    gripper_pose = Pose(position=Point(0,0,0.3),orientation=Quaternion(0,0.707,0,0.707))
-    # NOTE: in this project, we will only import 1 GraspableBody. Non-target objects
-    #       should be imported as an obstacle
-    gc.importObstacle('table', pose=table_pose)
-    gc.importGraspableBody('mug', pose=mug_pose) 
-    gc.importRobot('fetch_gripper',pose=gripper_pose)
-
-    print 'graspable bodies', gc.getGraspableBodies()
-    print 'robots', gc.getRobots()
-    while not rospy.is_shutdown():
-        gc.autoOpen()
-        rospy.sleep(0.5)
-        gc.autoGrasp()
-        rospy.sleep(0.5)
-
 class GraspitPrimitive(object):
     def __init__(self, modelname, pose):
         self.modelname = modelname
@@ -54,7 +32,4 @@ def demo2():
     pose = plan_grasp(GraspitPrimitive('fetch_gripper', gripper_pose),\
                       GraspitPrimitive('mug', mug_pose),\
                       [GraspitPrimitive('table', table_pose)])
-    print pose
     return pose
-if __name__ == '__main__':
-    demo2()
