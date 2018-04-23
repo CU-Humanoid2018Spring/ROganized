43e4a9c00d5991ba111c22a0d64d40d1600d9c4b
YanS0ng <c.yansong@columbia.edu>
2018-04-23 17:31:48 -0400

    feat: Integrate ImageConverter into data generation script

--- roganized_gazebo/scripts/generate_data.py
+++ roganized_gazebo/scripts/generate_data.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 from roganized_gazebo.table_manager import TableManager
+from roganized_rl.utils import ImageConverter
+import cv2
 import numpy as np
 import rospy
 if __name__ == '__main__':
+    img_src = ImageConverter()
     table = TableManager()
     table.clear()
     #grid = np.array([[1,0,0,0],
@@ -16,3 +19,8 @@ if __name__ == '__main__':
         for i, position in enumerate(positions):
             table.move_cube(i, position/4, position%4)
         rospy.sleep(0.2)
+        img = img_src.get_rgb()
+        if img is not None:
+            cv2.imshow('demo',img)
+            cv2.waitKey(3)
+     cv2.destroyAllWindow()
