0cccd80a77da8ad2a4eef3eb5069826863fbdd7a
Kelsey D'Souza <kdz@users.noreply.github.com>
2018-04-25 11:18:55 -0400

    Save .npy of table scores along with images

--- roganized_gazebo/scripts/generate_data.py
+++ roganized_gazebo/scripts/generate_data.py
@@ -43,7 +43,7 @@ def setup_base_dir(img_dir, data_path=DATA_PATH, batch_num=BATCH_START):
     
     
 def update_cur_dir(batch_num, img_dir):
-    """Create new directory for next batch of images. Returns updated batch_num, updated cur_dir."""
+    """Create new directory for next batch of images. Returns updated cur_dir path."""
     cur_dir = os.path.join(data_path, img_dir, "batch_" + str(batch_num))
     if not os.path.exists(cur_dir):
         os.makedirs(cur_dir)
@@ -52,7 +52,7 @@ def update_cur_dir(batch_num, img_dir):
     
 
 def same_img(img, ref):
-    """Check if cv2 images are identical."""
+    """Check if cv2 images are identical to filter out blank tables."""
     identical = (img.shape == ref.shape) and not (np.bitwise_xor(img, ref).any())
     # if identical:
     #     print("same image detected")
@@ -60,29 +60,34 @@ def same_img(img, ref):
     
 
 if __name__ == '__main__':
+    # Capture input args <img_dir> and <num_images>
     img_dir = sys.argv[1]
     count = 100 if len(sys.argv) < 2 else int(sys.argv[2])
-    print("Generating %i images" % count)
     
-    img_src = ImageConverter()
-    table = TableManager()
-    table.clear()
-    #grid = np.array([[1,0,0,0],
-    #                 [0,0,1,0],
-    #                 [1,0,0,0],
-    #                 [0,0,0,1]])
-    table.spawn()
-    
+    # Setup working paths
     data_path, batch_num = setup_base_dir(img_dir)
     cur_dir = update_cur_dir(batch_num, img_dir)
+    
+    print("Generating %i images in directory %s" % (count, os.join(data_path, img_dir)))
+    
+    # Setup blank table images to filter out
     ref_imgs = []
     for img_name in REFS:
         ref_path = os.path.join(data_path, img_name)
         print(ref_path)
         ref_imgs.append(cv2.imread(ref_path))
-    print(ref_imgs)
+    failed_imgs = [i for i, img in enumerate(ref_imgs) if img is None]
+    if len(failed_imgs) > 0:
+        print("The following reference images failed to load:", REFS[failed_imgs])
+    
+    # Initialize ROS managers
+    img_src = ImageConverter()
+    table = TableManager()
+    table.clear()
+    table.spawn()
 
     img_count = 0
+    scores = []
     while not rospy.is_shutdown() and img_count < count:
         n = len(os.listdir(cur_dir))
         
@@ -94,19 +99,24 @@ if __name__ == '__main__':
         # Stabilize
         rospy.sleep(0.2)
         
-        # Create a new batch image directory if needed
+        # Create a new batch image directory if needed, saving .npy file first.
         if img_count > 1 and n % BATCH_SIZE == 0:
+            scores_path = os.join(cur_dir, "batch_%i_scores" % batch_num)
+            np.save(scores_path, scores)
+            scores = []
             batch_num += 1
             cur_dir = update_cur_dir(batch_num, img_dir)
             
-        # Get img
+        # Get img and score
         img = img_src.get_rgb()
+        score = table.score()
         if img is None:
             continue
         elif sum([same_img(img, ref) for ref in ref_imgs]) == 0:
             img_name = prefix + str(img_count) + suffix
             img_path = os.path.join(cur_dir, img_name)
             cv2.imwrite(img_path, img)
+            scores.append(score)
             img_count += 1
 
     print("Generated %i images in %s" % (img_count, data_path))
